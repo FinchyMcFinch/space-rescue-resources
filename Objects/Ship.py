05740043d438583998af66bc968262b0d51ff3f4
FinchyMcFinch <alex@2am.com.au>
2024-02-09 15:07:13 +1000

    ship movement upgrades

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,6 +1,6 @@
 # Ship.py
 
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 
 class Ship(RoomObject):
@@ -15,12 +15,44 @@ class Ship(RoomObject):
 
         self.handle_key_events = True
 
+        self.movement_speed = 2
+
     def key_pressed(self, key):
         """
         vertical movement
         """
         if key[pygame.K_w]:
-            self.y_speed = -10
+            self.y_speed -= self.movement_speed
         
         elif key[pygame.K_s]:
-            self.y_speed = 10
+            self.y_speed += self.movement_speed
+
+    def step(self):
+        """
+        game tick
+        """
+        self.keep_in_room()
+        if not self.check_key(pygame.K_w) and self.y_speed < 0:
+
+
+    def check_key(self, key_id):
+        """
+        check if given key is pressed
+        """
+        flag = False
+        for event in pygame.event.get:
+            if event.type == pygame.KEYDOWN:
+                if event.key == key_id:
+                    flag = True
+        if flag:
+            return True
+        return False
+
+    def keep_in_room(self):
+        """
+        check for collision with border
+        """
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height > Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
