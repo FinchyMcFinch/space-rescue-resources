40a61e127333b34f30a1b23006ae3f91e552adfe
FinchyMcFinch <alex@2am.com.au>
2024-02-21 14:13:22 +1000

    the big one

---
+++ Objects/Asteroid.py
@@ -0,0 +1,47 @@
+# Asteroid.py
+
+from GameFrame import RoomObject, Globals
+import random
+
+class Asteroid(RoomObject):
+    """
+    big flying rock
+    """
+    def __init__(self, room, x, y):
+        RoomObject.__init__(self, room, x, y)
+
+        dimension = random.choice([40, 50, 60, 75])
+        if random.randint (1,100) <= 3:
+            dimension = 175
+            self.x -= 100
+            speed = 3
+        self.set_image(self.load_image("asteroid.png"), dimension, dimension)
+
+        ## travel direction
+        speed = (10*50)/self.width
+        self.set_direction(random.randint(135, 225), speed)
+
+    def step(self):
+        """
+        game tick
+        """
+        self.keep_in_room()
+        self.outside_room()
+    
+    def keep_in_room(self):
+        """
+        check for collision with border
+        """
+        if self.y < 0:
+            self.y = 0
+            self.y_speed *= -1
+        elif self.y > Globals.SCREEN_HEIGHT - self.height:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+            self.y_speed *= -1
+    
+    def outside_room(self):
+        """
+        prune old asteroids
+        """
+        if self.x + self.width < 0:
+            self.room.delete_object(self)
--- Objects/Zork.py
+++ Objects/Zork.py
@@ -1,8 +1,9 @@
 # Zork.py
 
 from GameFrame import RoomObject, Globals
+from Objects.Asteroid import Asteroid
 import random
-import math
+
 
 class Zork(RoomObject):
     """
@@ -16,6 +17,9 @@ class Zork(RoomObject):
         self.movement_speed = (10 * 30)/Globals.FRAMES_PER_SECOND ## numbers in brackets are desired speed at given fps
         self.y_speed = random.choice([-self.movement_speed, self.movement_speed])
         self.turnaround_chance = 0.4
+
+        asteroid_spawn_time = 35
+        self.set_timer(asteroid_spawn_time, self.spawn_asteroid)
 
     def keep_in_room(self):
         """
@@ -34,3 +38,13 @@ class Zork(RoomObject):
         chance = round(random.random()*100,2)
         if chance < self.turnaround_chance:
             self.y_speed *= -1
+
+    def spawn_asteroid(self):
+        """
+        randomly spawns big rock
+        """
+        new_asteroid = Asteroid(self.room, self.x, self.y + self.height/2)
+        self.room.add_room_object(new_asteroid)
+
+        asteroid_spawn_time = random.randint(20, 250)
+        self.set_timer(asteroid_spawn_time, self.spawn_asteroid)
